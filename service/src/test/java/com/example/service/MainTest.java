f99bfe60b8bf9e15626ba2d94f6accac6ae2bb40
Ubuntu <DemoVM@DemoVM.wlj33uwlrnherehffafemkjugg.rx.internal.cloudapp.net>
2025-12-04 13:53:03 +0000

    4th coommit

---
+++ service/src/test/java/com/example/service/MainTest.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void sampleTest() {
+        int a = 2 + 3;
+        assertEquals(5, a);
+    }
+}
