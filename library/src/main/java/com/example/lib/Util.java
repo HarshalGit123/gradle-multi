e9f82285a713f8e64c7f6b5d7b3d2c4a3a07139d
Ubuntu <DemoVM@DemoVM.wlj33uwlrnherehffafemkjugg.rx.internal.cloudapp.net>
2025-12-04 12:53:34 +0000

    1st commit

---
+++ library/src/main/java/com/example/lib/Util.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+
+public class Util {
+    public static String greet() {
+        return "Hello from Library!";
+    }
+}
+
---
+++ service/src/main/java/com/example/service/Main.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.lib.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Util.greet());
+    }
+}
